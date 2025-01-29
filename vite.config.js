403de110cb9bd98dc302efceff0e016898564d3d
debjitgoswami <debjitg421@gmail.com>
2025-01-29 23:41:03 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/DebjitPortfolio/", // Correct the base URL with a leading and trailing slash
   plugins: [react()],
-})
+});
